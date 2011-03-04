061c57f463e5a894647b8026215e5ff1b2ca81da
matthias.liechti <matthias.liechti@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-03-04 17:05:34 +0000

    Initial

---
+++ trunk/ch.jester.commonservices.api/src/ch/jester/commonservices/api/ILogger.java
@@ -0,0 +1,13 @@
+package ch.jester.commonservices.api;
+
+import org.eclipse.core.runtime.ILog;
+
+public interface ILogger extends ILog {
+public void info(String pMessage, Throwable pThrowable);
+public void info(String pMessage);
+public void debug(String pMessage);
+public void error(String pError);
+public void error(Throwable pThrowable);
+public void error(String pMessage, Throwable pThrowable);
+public ILog getLog();
+}
