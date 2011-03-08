223bb6868ccdc0177ffc805ea8374a4c74d9a272
nomisp@gmail.com <nomisp@gmail.com@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-03-08 21:23:20 +0000

    Datenmodell

--- ch.jester.model/src/ch/jester/model/Round.java
+++ ch.jester.model/src/ch/jester/model/Round.java
@@ -1,6 +1,8 @@
 package ch.jester.model;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,12 +22,55 @@ public class Round implements Serializable {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	
-	@Column(name="RoundNumber")
+	@Column(name="RoundNumber", nullable=false)
 	private int number;
 	
 	@ManyToOne
 	private Category category;
 	
-	
+	@OneToMany(mappedBy="round")
+	private Set<Pairing> pairings = new HashSet<Pairing>();
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Set<Pairing> getPairings() {
+		return pairings;
+	}
+
+	public void setPairings(Set<Pairing> pairings) {
+		this.pairings = pairings;
+	}
+	
+	public void addPairing(Pairing pairing) {
+		if (pairing == null) throw new IllegalArgumentException("pairing may not be null");
+		this.pairings.add(pairing);
+		if (pairing.getRound() != this) pairing.setRound(this);
+	}
+
+	public void removePairing(Pairing pairing) {
+		if (pairing == null) throw new IllegalArgumentException("pairing may not be null");
+		this.pairings.remove(pairing);
+	}
 }
