566ec7b59e651a1d898ce0f5125097aa4c45be6b
matthias.liechti <matthias.liechti@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-03-12 09:00:15 +0000

    Refactoring und DB Zeugs

--- trunk/ch.jester.hibernate.helper/src/ch/jester/hibernate/helper/HibernatehelperPlugin.java
+++ trunk/ch.jester.hibernate.helper/src/ch/jester/hibernate/helper/HibernatehelperPlugin.java
@@ -1,44 +1,43 @@
 package ch.jester.hibernate.helper;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IContributor;
-import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
 import ch.jester.common.activator.AbstractActivator;
+import ch.jester.common.utility.ExtensionPointUtil;
 import ch.jester.commonservices.api.logging.ILogger;
 
 public class HibernatehelperPlugin extends AbstractActivator {
-	ILogger logger;
-	IDatabaseManager mDBManager;
+	//ExtensionPoint Name
+	private String EP_CONFIGURATION = "Configuration";
+	//Attribute des ExtensionPoints
+	private final static String EP_CONFIGURATION_DATABASEMANAGERCLZ = "DatabaseManagerClass";
+	private ILogger mLogger;
+	private IDatabaseManager mDBManager;
 	//The shared instance.
 	private static HibernatehelperPlugin plugin;
 	
-	/**
-	 * The constructor.
-	 */
 	public HibernatehelperPlugin() {
 		plugin = this;
 	}
 
-	/**
-	 * This method is called upon plug-in activation
-	 */
+	@Override
 	public void startDelegate(BundleContext context) {
-		logger = getActivationContext().getLogger();
-		logger.info("HibernatehelperPlugin started");
+		mLogger = getActivationContext().getLogger();
+		mLogger.info("HibernatehelperPlugin started");
 		startHSQLDB();
+
 	}
 
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
+	@Override
 	public void stopDelegate(BundleContext context) {
-		if(mDBManager!=null){
-			mDBManager.stop();
+		
+		if(mDBManager!=null){	
 			mDBManager.shutdown();
+			mDBManager.stop();
 		}
 	}
 
@@ -48,62 +47,28 @@ public class HibernatehelperPlugin extends AbstractActivator {
 	public static HibernatehelperPlugin getDefault() {
 		return plugin;
 	}
-	
-	/**
-	 * liefert das ConfigurationElment, das f�r den angegebenen ExtensionPoint mit
-	 * dem angegebenen Namen deklariet ist.
-	 * Wenn kein Element gefunden werden kann, wird null geliefert
-	 * Der Extensionpoint mu� von diesem Plugin deklariert worden sein
-	 * und ein anderes Plugin h�ngt sich dort ein und setzt den konkreten Wert
-	 * Dessen Element wird dann geliefert 
-	 * @param extensionPointId
-	 * @return
-	 */
-	public  IConfigurationElement getExtensionPointElement(String extensionPointId) {
-		// die Id des RCP-Plugins ermitteln
-		String pluginid = getId();
-		IExtensionRegistry registry = Platform.getExtensionRegistry();
-		IConfigurationElement[] elements = registry.getConfigurationElementsFor(pluginid, extensionPointId);	
-		for (IConfigurationElement element : elements) {
-			// Element mit dem angegebenen Namen liefern
-			String name = element.getName();			
-			if (name.equals(extensionPointId)) {				
-				return element;
-			}
-		}
-		// nix gefunden, null liefern
-		return null;
-	}
-	
 	/**
 	 * liefert die Id dieses Plugins aus dem Manifest
 	 * @return
 	 */
-	public String getId() {
+	public String getPluginId() {
 		return getActivationContext().getPluginId();
 	}
 	
 	private void startHSQLDB() {
-		
-		IConfigurationElement element = getExtensionPointElement("Configuration");
-		String dbmClassName = element.getAttribute("DatabaseManagerClass");
-		
-		IContributor contributor = element.getContributor();
-		Bundle databaseBundle = Platform.getBundle(contributor.getName());
-		getActivationContext().getLogger().info("DatabaseManagerClass is "+dbmClassName+" located in Bundle: "+databaseBundle);
+		IConfigurationElement element = ExtensionPointUtil.getExtensionPointElement(getPluginId(),EP_CONFIGURATION);
+		String dbmClassName = element.getAttribute(EP_CONFIGURATION_DATABASEMANAGERCLZ);
 		if (dbmClassName != null) {
 			try {
-				Class<IDatabaseManager> c = databaseBundle.loadClass(dbmClassName);
-				mDBManager = (IDatabaseManager) c.newInstance();
+				Bundle databaseBundle = Platform.getBundle(element.getContributor().getName());
+				getActivationContext().getLogger().info("DatabaseManagerClass is "+dbmClassName+" located in Bundle: "+databaseBundle);
+				mDBManager =  (IDatabaseManager) element.createExecutableExtension(EP_CONFIGURATION_DATABASEMANAGERCLZ);
 				mDBManager.start();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
+		
 	}
-
+	}
 }
