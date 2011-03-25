6de7c5cb0b5078a86519245777ca694ecabfadc0
matthias.liechti <matthias.liechti@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-03-25 15:25:44 +0000

    nicht benötigt

--- trunk/ch.jester.ormapper/src/ch/jester/ormapper/api/IDatabaseDefinition.java
+++
@@ -1,8 +0,0 @@
-package ch.jester.ormapper.api;
-
-public interface IDatabaseDefinition {
-	public String getDatabaseName();
-	public String getJDBCDriverClass();
-	public String getSubprotocol();
-	public IDatabaseManager getDatabaseManager();
-}
