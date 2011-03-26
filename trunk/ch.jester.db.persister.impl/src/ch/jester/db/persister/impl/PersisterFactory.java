956d13d396089a217e1a00f8b78c8f462f8d0f3d
matthias.liechti <matthias.liechti@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-03-26 07:28:52 +0000

    javadoc

--- trunk/ch.jester.db.persister.impl/src/ch/jester/db/persister/impl/PersisterFactory.java
+++ trunk/ch.jester.db.persister.impl/src/ch/jester/db/persister/impl/PersisterFactory.java
@@ -13,6 +13,15 @@ import ch.jester.common.utility.ServiceUtility;
 import ch.jester.commonservices.api.IComponentService;
 import ch.jester.dao.IPlayerPersister;
 
+/**
+ * Implementiert eine ServiceFactory und stellt die DAO Implementierungen zur Verfügung.
+ * Die Klasse wird als Komponente mit DS gestartet und registriert sich selbst als Factory für die entsprechenden
+ * DAO Services.
+ * Die Factory gibt immer eine neue Service Instanz zurück. Damit wird das DS/OSGI Caching pro Bundle umgangen, so dass
+ * der DAO Service ansich nicht Threadsafe sein muss.
+ *  
+ *
+ */
 public class PersisterFactory implements ServiceFactory, IComponentService<Object>{
 	private ServiceUtility mServiceUtility;
 	private HashMap<String, Class<?>> mRegistry = new HashMap<String, Class<?>>();
@@ -46,6 +55,11 @@ public class PersisterFactory implements ServiceFactory, IComponentService<Objec
 		
 	}
 
+	/**
+	 * Registriert eine implementierende Klasse und deren Service Interface
+	 * @param pInterfaceClassName das eigentlich Service Interface
+	 * @param class1 die Implementierende Klasse
+	 */
 	public void addServiceHandling(Class<?> pInterfaceClassName, Class<?> class1) {
 		mRegistry.put(pInterfaceClassName.getName(), class1);
 		mServiceUtility.registerServiceFactory(pInterfaceClassName, this);
