91084998020aac05cd20b25422a750dac94b421f
nomisp@gmail.com <nomisp@gmail.com@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-04-05 20:13:10 +0000

    Help-Plugin

--- trunk/ch.jester.rcpapplication/src/ch/jester/rcpapplication/ApplicationWorkbenchAdvisor.java
+++ trunk/ch.jester.rcpapplication/src/ch/jester/rcpapplication/ApplicationWorkbenchAdvisor.java
@@ -1,5 +1,11 @@
 package ch.jester.rcpapplication;
 
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.ui.IMemento;
+import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
@@ -17,5 +23,31 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 	public String getInitialWindowPerspectiveId() {
 		return PERSPECTIVE_ID;
 	}
+	
+	/**
+	 * Zustand der Workbench speichern 
+	 */
+	@Override
+    public void initialize(IWorkbenchConfigurer configurer) {
+		super.initialize(configurer);
+        configurer.setSaveAndRestore(true);
+    }
+
+    @Override
+    public IStatus saveState(IMemento memento) {
+        memento.createChild("jester").putString("lastOpenedDate", 
+        		DateFormat.getDateTimeInstance().format(new Date()));
+        return super.saveState(memento);
+    }
+
+    @Override
+    public IStatus restoreState(IMemento memento) {
+        if (memento != null) {
+            IMemento jesterMemento = memento.getChild("jester");
+            if (jesterMemento != null)
+                System.out.println("Last opened on: " + jesterMemento.getString("lastOpenedDate"));
+        }
+        return super.restoreState(memento);
+    }
 
 }
