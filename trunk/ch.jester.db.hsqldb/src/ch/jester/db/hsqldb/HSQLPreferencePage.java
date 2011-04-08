810054f25b7416a1c08ec3276cc4558c6f0ee0b2
nomisp@gmail.com <nomisp@gmail.com@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-04-08 14:48:17 +0000

    Datenbank Preferences

--- trunk/ch.jester.db.hsqldb/src/ch/jester/db/hsqldb/HSQLPreferencePage.java
+++ trunk/ch.jester.db.hsqldb/src/ch/jester/db/hsqldb/HSQLPreferencePage.java
@@ -1,21 +1,40 @@
 package ch.jester.db.hsqldb;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 public class HSQLPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
+	public HSQLPreferencePage() {
+		super(GRID);
+	}
+	
 	@Override
 	public void init(IWorkbench workbench) {
-		// TODO Auto-generated method stub
-		
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		String text = Platform.getPreferencesService().getString("ch.jester.orm", "Database", "ch.jester.db.hsqldb", null);
+		System.out.println(text);
 	}
 
 	@Override
 	protected void createFieldEditors() {
-		// TODO Auto-generated method stub
+		StringFieldEditor defTableTypeEditor = new StringFieldEditor("DefaultTableType", "Table-Type:", getFieldEditorParent());
+		defTableTypeEditor.setStringValue(Platform.getPreferencesService().getString("ch.jester.db.hsqldb", "DefaultTableType", "cached", null));
+		defTableTypeEditor.setEnabled(false, getFieldEditorParent());
+		addField(defTableTypeEditor);
+
+		StringFieldEditor transactionModeEditor = new StringFieldEditor("TransactionMode", "Transaktionsmodus:", getFieldEditorParent());
+		transactionModeEditor.setStringValue(Platform.getPreferencesService().getString("ch.jester.db.hsqldb", "TransactionMode", "mvcc", null));
+		transactionModeEditor.setEnabled(false, getFieldEditorParent());
+		addField(transactionModeEditor);
 		
+		StringFieldEditor ipEditor = new StringFieldEditor("Ip", "Ip-Adresse:", getFieldEditorParent());
+		ipEditor.setStringValue(Platform.getPreferencesService().getString("ch.jester.db.hsqldb", "Ip", "localhost", null));
+		ipEditor.setEnabled(false, getFieldEditorParent());
+		addField(ipEditor);
 	}
 
 }
--- trunk/ch.jester.orm/src/ch/jester/orm/DatabasePreferencePage.java
+++ trunk/ch.jester.orm/src/ch/jester/orm/DatabasePreferencePage.java
@@ -1,14 +1,22 @@
 package ch.jester.orm;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
+import ch.jester.commonservices.api.logging.ILogger;
+
 public class DatabasePreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 	
+	private ILogger logger;
+
 	public DatabasePreferencePage() {
 		super(GRID);
+		logger = ORMPlugin.getDefault().getActivationContext().getLogger();
 	}
 	
 	@Override
@@ -19,8 +27,39 @@ public class DatabasePreferencePage extends FieldEditorPreferencePage implements
 
 	@Override
 	protected void createFieldEditors() {
-		addField(new ComboFieldEditor("Database", ORMMessages.DatabasePreferencePage_DatabaseLabel, new String[][] { { "HSQLDB", "ch.jester.db.hsqldb" }, //$NON-NLS-1$ //$NON-NLS-3$ //$NON-NLS-4$
+		IPreferenceStore preferenceStore = ORMPlugin.getDefault().getPreferenceStore();
+		String databasePlugin = preferenceStore.getString("Database");	// Konkretes Datenbankplugin aus welchem die Konfiguration gelesen werden soll.
+		
+		logger.info("Selected Database: " + databasePlugin);
+		
+		addField(new ComboFieldEditor("Database", ORMMessages.DatabasePreferencePage_DatabaseLabel, new String[][] { { "HSQLDB", "ch.jester.db.hsqldb" }, //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-2$
 				{ "C&hoice 2", "choice2" } }, getFieldEditorParent())); //$NON-NLS-1$ //$NON-NLS-2$
+		StringFieldEditor dbNameEditor = new StringFieldEditor("DatabaseName", ORMMessages.DatabasePreferencePage_DatabaseName, getFieldEditorParent()); //$NON-NLS-1$
+		dbNameEditor.setStringValue(preferenceStore.getString("DatabaseName"));
+		addField(dbNameEditor);
+		
+		String jdbcDriver = Platform.getPreferencesService().getString(databasePlugin, "JDBCDriverClass", "org.hsqldb.jdbcDriver", null);
+		StringFieldEditor jdbcDriverEditor = new StringFieldEditor("JDBCDriverClass", ORMMessages.DatabasePreferencePage_JDBCDriver, getFieldEditorParent()); //$NON-NLS-1$
+		jdbcDriverEditor.setStringValue(jdbcDriver);
+		jdbcDriverEditor.setEnabled(false, getFieldEditorParent());
+		addField(jdbcDriverEditor);
+		
+		String sqlDialect = Platform.getPreferencesService().getString(databasePlugin, "SQLDialectClass", "org.hibernate.dialect.HSQLDialect", null);
+		StringFieldEditor sqlDialectEditor = new StringFieldEditor("SQLDialectClass", ORMMessages.DatabasePreferencePage_SQLDialect, getFieldEditorParent()); //$NON-NLS-1$
+		sqlDialectEditor.setStringValue(sqlDialect);
+		sqlDialectEditor.setEnabled(false, getFieldEditorParent());
+		addField(sqlDialectEditor);
+		
+		String subProtocol = Platform.getPreferencesService().getString(databasePlugin, "Subprotocol", "hsqldb", null);
+		StringFieldEditor subProtocolEditor = new StringFieldEditor("Subprotocol", ORMMessages.DatabasePreferencePage_Subprotocol, getFieldEditorParent()); //$NON-NLS-1$
+		subProtocolEditor.setStringValue(subProtocol);
+		subProtocolEditor.setEnabled(false, getFieldEditorParent());
+		addField(subProtocolEditor);
+		
+//		addField(new StringFieldEditor("DatabaseManagerClass", ORMMessages.DatabasePreferencePage_DatabaseManager, //$NON-NLS-1$
+//				getFieldEditorParent()));
+//		addField(new StringFieldEditor("ORMConfiguration", ORMMessages.DatabasePreferencePage_ORMConfiguration, //$NON-NLS-1$
+//				getFieldEditorParent()));
 	}
 
 }
