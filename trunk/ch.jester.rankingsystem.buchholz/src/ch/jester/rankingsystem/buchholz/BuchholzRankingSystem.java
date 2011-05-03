3dc09132cf3d440b169223c248ac4181889d74a6
nomisp@gmail.com <nomisp@gmail.com@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-05-03 20:17:23 +0000

    Plugin für Feinwertung nach Buchholz.

---
+++ trunk/ch.jester.rankingsystem.buchholz/src/ch/jester/rankingsystem/buchholz/BuchholzRankingSystem.java
@@ -0,0 +1,29 @@
+package ch.jester.rankingsystem.buchholz;
+
+import java.util.List;
+
+import ch.jester.model.Category;
+import ch.jester.model.Player;
+import ch.jester.system.api.ranking.IRankingSystem;
+import ch.jester.system.exceptions.NotAllResultsException;
+
+/**
+ * Feinwertung nach Buchholz
+ * @author Peter
+ *
+ */
+public class BuchholzRankingSystem implements IRankingSystem {
+
+	@Override
+	public List<Player> calculateRanking() throws NotAllResultsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Player> calculateRanking(Category category) throws NotAllResultsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
