6db17e67100e4a1c86beec3e0668b6f65a6162d4
nomisp@gmail.com <nomisp@gmail.com@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-05-21 19:37:28 +0000

    Hinzufügen von Spielern zu einer Kategorie

--- trunk/ch.jester.ui.tournament/src/ch/jester/ui/tournament/handler/AddPlayerHandler.java
+++ trunk/ch.jester.ui.tournament/src/ch/jester/ui/tournament/handler/AddPlayerHandler.java
@@ -3,18 +3,36 @@ package ch.jester.ui.tournament.handler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.ui.IPersistableElement;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.navigator.CommonNavigator;
 
 import ch.jester.common.ui.editor.IEditorDaoInputAccess;
 import ch.jester.common.ui.handlers.AbstractCommandHandler;
+import ch.jester.commonservices.api.persistency.IDaoService;
+import ch.jester.dao.ICategoryDao;
+import ch.jester.dao.IPlayerDao;
 import ch.jester.model.Category;
+import ch.jester.model.Player;
+import ch.jester.ui.tournament.cnf.TournamentNavigator;
+import ch.jester.ui.tournament.internal.Activator;
 
+/**
+ * Handler um Spieler zu einer Kategorie zuzuordnen
+ * @author Peter
+ *
+ */
 public class AddPlayerHandler extends AbstractCommandHandler implements IHandler {
 
+	private Category selectedCategory;
+	
 	@Override
 	public Object executeInternal(ExecutionEvent event) {
 		//alternative(event);
-//		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
 //		IWorkbenchPage page = window.getActivePage();
 //		ISelection selection = getSelection();
 //		if (!selection.isEmpty()) {
@@ -34,9 +52,43 @@ public class AddPlayerHandler extends AbstractCommandHandler implements IHandler
 //		}
 		Category cat = getFirstSelectedAs(Category.class);
 		if(cat!=null){
-			openEditor(cat);
+			selectedCategory = cat;
+//			openEditor(cat);
+			ElementListSelectionDialog dialog = new ElementListSelectionDialog(window.getShell(), new PlayerLabelProvider());
+			dialog.setTitle("Select Players");
+			dialog.setMessage("Select Players (Ctrl+Click for multiple selection)");
+			dialog.setElements(getPlayers());
+			dialog.setMultipleSelection(true);
+			dialog.open();
+			Object[] result = dialog.getResult();
+			wirePlayers(result);
+			CommonNavigator cn = (CommonNavigator)getView(TournamentNavigator.ID);
+			cn.getCommonViewer().refresh();
 		}
 		return null;
+	}
+	
+	/**
+	 * Holen der Spieler aus der DB
+	 * @return
+	 */
+	private Object[] getPlayers() {
+		IDaoService<Player> pdao= Activator.getDefault().getActivationContext().getService(IPlayerDao.class);
+		
+		return pdao.getAll().toArray();
+	}
+	
+	/**
+	 * Verknüpfen der Spieler mit der Kategorie
+	 * @param selection
+	 */
+	private void wirePlayers(Object[] selection) {
+		for (Object object : selection) {
+			Player p = (Player)object;
+			selectedCategory.addPlayer(p);
+		}
+		IDaoService<Category> catDao= Activator.getDefault().getActivationContext().getService(ICategoryDao.class);
+		catDao.save(selectedCategory);
 	}
 	
 	public class CategoryEditorInput implements IEditorDaoInputAccess<Category> {
@@ -118,6 +170,23 @@ public class AddPlayerHandler extends AbstractCommandHandler implements IHandler
 		public boolean isAlreadyDirty() {
 			// TODO Auto-generated method stub
 			return false;
+		}
+	}
+	
+	/**
+	 * Label Provider für die Spieler
+	 * @author Peter
+	 *
+	 */
+	public class PlayerLabelProvider extends LabelProvider {
+		@Override
+		public String getText(Object element) {
+			Player p = (Player)element;
+			StringBuffer sb = new StringBuffer();
+			sb.append(p.getFirstName());
+			sb.append(" ");
+			sb.append(p.getLastName());
+			return  sb.toString();
 		}
 	}
 }
