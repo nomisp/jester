bed4d7440d8c226f326dfa23987e720c41428c98
nomisp@gmail.com <nomisp@gmail.com@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-05-21 18:21:20 +0000

    Deutsche Übersetzung ins Product eingetragen

--- trunk/ch.jester.ui.tournament/src/ch/jester/ui/tournament/handler/AddPlayerHandler.java
+++ trunk/ch.jester.ui.tournament/src/ch/jester/ui/tournament/handler/AddPlayerHandler.java
@@ -2,63 +2,44 @@ package ch.jester.ui.tournament.handler;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IPersistableElement;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 import ch.jester.common.ui.editor.IEditorDaoInputAccess;
 import ch.jester.common.ui.handlers.AbstractCommandHandler;
-import ch.jester.common.ui.services.IEditorService;
-import ch.jester.common.ui.utility.UIUtility;
 import ch.jester.model.Category;
-import ch.jester.ui.tournament.editors.WirePlayerEditor;
 
 public class AddPlayerHandler extends AbstractCommandHandler implements IHandler {
 
 	@Override
 	public Object executeInternal(ExecutionEvent event) {
 		//alternative(event);
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
-		IWorkbenchPage page = window.getActivePage();
-		ISelection selection = getSelection();
-		if (!selection.isEmpty()) {
-			IStructuredSelection sSelection = (IStructuredSelection)selection;
-			if (sSelection.size() == 1 && sSelection.getFirstElement() instanceof Category) {
-				MessageDialog.openInformation(
-						HandlerUtil.getActiveWorkbenchWindow(event).getShell(), 
-						"AddPlayer", "Adding Player to " + ((Category)sSelection.getFirstElement()).getDescription());
-				Category cat = (Category)sSelection.getFirstElement();
-				CategoryEditorInput input = new CategoryEditorInput(cat.getId());
-				try {
-					page.openEditor(input, WirePlayerEditor.ID);
-				} catch (PartInitException e) {
-					e.printStackTrace();
-				}
-			}
+//		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+//		IWorkbenchPage page = window.getActivePage();
+//		ISelection selection = getSelection();
+//		if (!selection.isEmpty()) {
+//			IStructuredSelection sSelection = (IStructuredSelection)selection;
+//			if (sSelection.size() == 1 && sSelection.getFirstElement() instanceof Category) {
+//				MessageDialog.openInformation(
+//						HandlerUtil.getActiveWorkbenchWindow(event).getShell(), 
+//						"AddPlayer", "Adding Player to " + ((Category)sSelection.getFirstElement()).getDescription());
+//				Category cat = (Category)sSelection.getFirstElement();
+//				CategoryEditorInput input = new CategoryEditorInput(cat.getId());
+//				try {
+//					page.openEditor(input, WirePlayerEditor.ID);
+//				} catch (PartInitException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+		Category cat = getFirstSelectedAs(Category.class);
+		if(cat!=null){
+			openEditor(cat);
 		}
 		return null;
 	}
-
-	/**Alternative zu obigen Aufruf, da ja der EditorService im Activator schon an die
-	 * Cat gebunden ist, gehts auch einfacher
-	 * @param event
-	 */
-	private void alternative(ExecutionEvent event) {
-		Category cat = getFirstSelectedAs(Category.class);
-		if(cat!=null){
-			openEditor(cat, WirePlayerEditor.ID);
-		}
-		
-	}
-
-	public class CategoryEditorInput implements IEditorDaoInputAccess {
+	
+	public class CategoryEditorInput implements IEditorDaoInputAccess<Category> {
 		
 		private final Integer id;
 		
@@ -122,13 +103,13 @@ public class AddPlayerHandler extends AbstractCommandHandler implements IHandler
 	    }
 
 		@Override
-		public Object getInput() {
+		public Category getInput() {
 			// TODO Auto-generated method stub
 			return null;
 		}
 
 		@Override
-		public void setInput(Object pT) {
+		public void setInput(Category pT) {
 			// TODO Auto-generated method stub
 			
 		}
