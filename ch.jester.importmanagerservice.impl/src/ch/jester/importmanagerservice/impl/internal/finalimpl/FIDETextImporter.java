b60b218e86edfebbe0c604499a417b0aae2d0aba
matthias.liechti <matthias.liechti@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-05-31 17:43:27 +0000

    Vorbereitungen für Duplicates detect beim importieren

--- ch.jester.importmanagerservice.impl/src/ch/jester/importmanagerservice/impl/internal/finalimpl/FIDETextImporter.java
+++ ch.jester.importmanagerservice.impl/src/ch/jester/importmanagerservice/impl/internal/finalimpl/FIDETextImporter.java
@@ -1,82 +1,23 @@
 package ch.jester.importmanagerservice.impl.internal.finalimpl;
 
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.persistence.Query;
-
-
-import ch.jester.common.importer.AbstractTableImporter;
 import ch.jester.common.importer.VirtualCell;
-import ch.jester.common.utility.SubListIterator;
 import ch.jester.commonservices.api.importer.IVirtualTable;
 import ch.jester.commonservices.api.importer.IVirtualTable.IVirtualCell;
-import ch.jester.commonservices.api.persistency.IDaoService;
-import ch.jester.commonservices.util.ServiceUtility;
 import ch.jester.importmanagerservice.impl.abstracts.FixTextTableProvider;
-import ch.jester.model.Player;
-import ch.jester.model.factories.ModelFactory;
 
-public class FIDETextImporter extends AbstractTableImporter<String, Player>{
-	private ServiceUtility su = new ServiceUtility();
-	
+public class FIDETextImporter extends AbstractPlayerImporter<String>{
+
 	public FIDETextImporter(){
 		init_linking();
 	}
 	private void init_linking() {
-		super.mInputLinking.put("lastName","LastName");
-		super.mInputLinking.put("firstName","FirstName");
-		super.mInputLinking.put("fideCode","CodeFIDE");
+		mInputLinking.put("lastName","LastName");
+		mInputLinking.put("firstName","FirstName");
+		mInputLinking.put("fideCode","CodeFIDE");
 		
 	}
-	@Override
-	protected void persist(List<Player> pDomainObjects) {
-		SubListIterator<Player> iterator = new SubListIterator<Player>(pDomainObjects, 10000);
-		
-		IDaoService<Player> checker = su.getDaoService(Player.class);
-		boolean checkDoubleEntries = checker.count()>0;
-		Query fideQuery = checker.createQuery("SELECT player FROM Player player WHERE player.fideCode in (:fideCode)");
-		
-		
-		while(iterator.hasNext()){
-			List<Player> sublist = iterator.next();
-			IDaoService<Player> playerpersister = su.getDaoService(Player.class);
-			
-			if(checkDoubleEntries){
-				List<Integer> fideCodes = createFideList(sublist);
-				List<Player> duplicates = fideQuery.setParameter("fideCode", fideCodes).getResultList();
-				System.out.println("Duplicates found >>>>"+duplicates.size());
-				
-			}
-			playerpersister.save(sublist);
-			playerpersister.close();
-		}
-		checker.close();
-		
-	}
-	
-	
-	
-	private List<Integer> createFideList(List<Player> sublist) {
-		List<Integer> fide = new ArrayList<Integer>();
-		for(Player p:sublist){
-			fide.add(p.getFideCode());
-		}
-		return fide;
-	}
-	@Override
-	public String[] getDomainObjectAttributes() {
-		return new String[]{"lastName","firstName","fideCode","elo","age","city","nation"};
-	}
-	public void done(){
-		
-	}
-	@Override
-	protected Player createNewDomainObject() {
-		return ModelFactory.getInstance().createPlayer();
-	}
-
 
 	@Override
 	public IVirtualTable<String> initialize(InputStream pInputStream) {
@@ -94,33 +35,4 @@ public class FIDETextImporter extends AbstractTableImporter<String, Player>{
 		provider.addCell(cell);
 		return provider;
 	}
-
-
-	
-	/*@Override
-	protected Player createDomainObject(Properties playerProperties) {
-		String name = playerProperties.getProperty("Name").trim();
-		//String firstName = playerProperties.getProperty("Vorname").trim();
-		Player player = ModelFactory.getInstance().createPlayer();
-		if(name.indexOf(",")!=-1){
-			String sfn = name.substring(name.indexOf(",")+1).trim();
-			name = name.substring(0, name.indexOf(",")).trim();
-			player.setFirstName(sfn);
-		}
-		player.setLastName(name);
-		
-		String fidecode = playerProperties.getProperty("CodeFIDE");
-		if(fidecode.equals("")){
-			fidecode="0";
-		}
-		player.setFideCode(Integer.parseInt(fidecode.trim()));
-		String elo = playerProperties.getProperty("Elo neu");
-		if(elo==null||elo.equals("")){
-			elo="0";
-		}
-		player.setElo(Integer.parseInt(elo));
-		player.setNation("Schweiz");
-		return player;
-	}*/
-	
 }
--- ch.jester.importmanagerservice.impl/src/ch/jester/importmanagerservice/impl/internal/finalimpl/SSBExcelImporter2.java
+++ ch.jester.importmanagerservice.impl/src/ch/jester/importmanagerservice/impl/internal/finalimpl/SSBExcelImporter2.java
@@ -2,37 +2,20 @@ package ch.jester.importmanagerservice.impl.internal.finalimpl;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.Query;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-import ch.jester.common.importer.AbstractTableImporter;
-import ch.jester.common.utility.SubListIterator;
 import ch.jester.commonservices.api.importer.IVirtualTable;
-import ch.jester.commonservices.api.persistency.IDaoService;
-import ch.jester.commonservices.util.ServiceUtility;
 import ch.jester.importmanagerservice.impl.abstracts.ExcelSheetTableProvider;
 import ch.jester.model.Player;
-import ch.jester.model.factories.ModelFactory;
 
-public class SSBExcelImporter2 extends AbstractTableImporter<Row, Player>{
-	private ServiceUtility su = new ServiceUtility();
-
-	
+public class SSBExcelImporter2 extends AbstractPlayerImporter<Row>{
 	public SSBExcelImporter2(){
 		init_Linking();
 	}
 	
-
-	@Override
-	public String[] getDomainObjectAttributes() {
-		return new String[]{"lastName","firstName","fideCode","elo","age","city","nation"};
-	}
 	
 	private void init_Linking(){
 		mInputLinking.put("lastName", "Name");
@@ -49,46 +32,7 @@ public class SSBExcelImporter2 extends AbstractTableImporter<Row, Player>{
 		return true;
 	}
 	
-	@Override
-	protected void persist(List<Player> pDomainObjects) {
-		SubListIterator<Player> iterator = new SubListIterator<Player>(pDomainObjects, 10000);
-		
-		IDaoService<Player> checker = su.getDaoService(Player.class);
-		boolean checkDoubleEntries = checker.count()>0;
-		Query fideQuery = checker.createQuery("SELECT player FROM Player player WHERE player.fideCode in (:fideCode)");
-		
-		
-		while(iterator.hasNext()){
-			List<Player> sublist = iterator.next();
-			IDaoService<Player> playerpersister = su.getDaoService(Player.class);
-			
-			if(checkDoubleEntries){
-				List<Integer> fideCodes = createFideList(sublist);
-				List<Player> duplicates = fideQuery.setParameter("fideCode", fideCodes).getResultList();
-				System.out.println("Duplicates found >>>>"+duplicates.size());
-				
-			}
-			playerpersister.save(sublist);
-			playerpersister.close();
-		}
-		checker.close();
-		
-	}
 	
-	private List<Integer> createFideList(List<Player> sublist) {
-		List<Integer> fide = new ArrayList<Integer>();
-		for(Player p:sublist){
-			fide.add(p.getFideCode());
-		}
-		return fide;
-	}
-	
-
-	@Override
-	protected Player createNewDomainObject() {
-		return ModelFactory.getInstance().createPlayer();
-	}
-
 	@Override
 	public IVirtualTable<Row> initialize(InputStream pInputStream) {
 		ExcelSheetTableProvider provider = new ExcelSheetTableProvider();
@@ -96,45 +40,12 @@ public class SSBExcelImporter2 extends AbstractTableImporter<Row, Player>{
 			provider.setSheet(WorkbookFactory.create(pInputStream).getSheetAt(0));
 			return provider;
 		} catch (InvalidFormatException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
 	}
-
-
-
-
-	/*@Override
-	protected Player createDomainObject(Properties playerProperties) {
-		return createDomainObject2(playerProperties);
-		
-		String name = playerProperties.getProperty("Name");
-		String firstName = playerProperties.getProperty("Vorname");
-		if(name.equals("")&&firstName.equals("")){
-			return null;
-		}
-		Player player = ModelFactory.getInstance().createPlayer();
-		player.setLastName(playerProperties.getProperty("Name"));
-		player.setFirstName(playerProperties.getProperty("Vorname"));
-		String fidecode = playerProperties.getProperty("CodeFIDE");
-		if(fidecode.equals("")){
-			fidecode="0";
-		}
-		player.setFideCode(Integer.parseInt(fidecode));
-		String elo = playerProperties.getProperty("Elo neu");
-		if(elo.equals("")){
-			elo="0";
-		}
-		player.setElo(Integer.parseInt(elo));
-		player.setNation("Schweiz");
-		return player;
-	}*/
-
-
 
 
 }
