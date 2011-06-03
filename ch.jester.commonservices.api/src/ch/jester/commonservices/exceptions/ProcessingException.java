7c922163a0c257c94a1c658bb3f5bb0fe39b27b6
matthias.liechti <matthias.liechti@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-06-03 17:52:36 +0000

    Exception Handling Importer --> MessageBox in UI

---
+++ ch.jester.commonservices.api/src/ch/jester/commonservices/exceptions/ProcessingException.java
@@ -0,0 +1,13 @@
+package ch.jester.commonservices.exceptions;
+
+public class ProcessingException extends RuntimeException{
+	public ProcessingException(String pMsg){
+		super(pMsg);
+	}
+	public ProcessingException(Throwable pThrow){
+		super(pThrow);
+	}
+	public ProcessingException(String pMessage, Throwable pThrow){
+		super(pMessage, pThrow);
+	}
+}
