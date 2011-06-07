1943e931061d55efe89becec8432f93975bdc2dc
nomisp@gmail.com <nomisp@gmail.com@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-06-07 17:50:26 +0000

    Entitäten für die Settings vergessen

---
+++ trunk/ch.jester.model/src/ch/jester/model/SettingItem.java
@@ -0,0 +1,79 @@
+package ch.jester.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="SettingItem")
+public class SettingItem extends AbstractModelBean<SettingItem> {
+	private static final long serialVersionUID = -7737771947626375572L;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "TournamentId", referencedColumnName = "Id")
+	private Tournament tournament;
+	
+	@Column(name="RootClassName")
+	private String rootClassName;
+	
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "settingItem", orphanRemoval=true)
+    private Set<SettingItemValue> settingItemValues = new HashSet<SettingItemValue>();
+
+	public Tournament getTournament() {
+		return tournament;
+	}
+
+	public void setTournament(Tournament tournament) {
+		this.tournament = tournament;
+	}
+
+	public String getRootClassName() {
+		return rootClassName;
+	}
+
+	public void setRootClassName(String rootClassName) {
+		this.rootClassName = rootClassName;
+	}
+
+	public Set<SettingItemValue> getSettingItemValues() {
+		return settingItemValues;
+	}
+
+	public void setSettingItemValues(Set<SettingItemValue> settingItemValues) {
+		this.settingItemValues = settingItemValues;
+	}
+
+    public void addSettingItemValue(SettingItemValue settingItemValue) {
+        if (settingItemValue == null)
+            throw new IllegalArgumentException("settingItemValue Object is NULL");
+
+        if (settingItemValue.getSettingItem() == null || !settingItemValue.getSettingItem().equals(this)) {
+            settingItemValue.setSettingItem(this);
+        }
+
+        if (!this.settingItemValues.contains(settingItemValue)) {
+            this.settingItemValues.add(settingItemValue);
+        }
+    }
+
+    public void removeSettingItemValue(SettingItemValue settingItemValue) {
+        if (settingItemValue == null) throw new IllegalArgumentException("settingItemValue Object is NULL");
+
+        if (settingItemValue.getSettingItem() != null && settingItemValue.getSettingItem().equals(this)) {
+            this.settingItemValues.remove(settingItemValue);
+            settingItemValue.setSettingItem(null);
+        }
+    }
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException();
+	}
+}
