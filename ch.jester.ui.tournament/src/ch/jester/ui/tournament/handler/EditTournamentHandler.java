6a6d29ad39c99d5f5d22ed0b70113ca0aa1bcb34
nomisp@gmail.com <nomisp@gmail.com@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-06-09 21:13:26 +0000

    Übersetzungen für den Turnier-Navigator, Handler für Turniereinstellungen (leerimpl.)

---
+++ ch.jester.ui.tournament/src/ch/jester/ui/tournament/handler/EditTournamentHandler.java
@@ -0,0 +1,18 @@
+package ch.jester.ui.tournament.handler;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+
+import ch.jester.common.ui.handlers.AbstractCommandHandler;
+
+public class EditTournamentHandler extends AbstractCommandHandler implements IHandler {
+
+	@Override
+	public Object executeInternal(ExecutionEvent event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
