5379e915f9dcd11f6846b8ce4b23893d13418f1d
matthias.liechti <matthias.liechti@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-07-03 18:07:11 +0000

    Layouts

--- trunk/ch.jester.ui.importer/src/ch/jester/ui/importer/AddColumnDialog.java
+++ trunk/ch.jester.ui.importer/src/ch/jester/ui/importer/AddColumnDialog.java
@@ -38,6 +38,8 @@ import ch.jester.common.importer.VirtualCell;
 import ch.jester.common.ui.adapters.TableLabelProviderAdapter;
 import ch.jester.common.ui.utility.SelectionUtility;
 import ch.jester.commonservices.api.importer.IVirtualTable.IVirtualCell;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.GridData;
 
 public class AddColumnDialog extends Dialog implements SelectionListener {
 	private static int colcounter = 1;
@@ -72,169 +74,176 @@ public class AddColumnDialog extends Dialog implements SelectionListener {
 	@Override
 	protected Control createDialogArea(Composite parent) {
 		Composite container = (Composite) super.createDialogArea(parent);
-		container.setLayout(null);
+		container.setLayout(new GridLayout(2, true));
 
 		Label lblInputSelection = new Label(container, SWT.NONE);
-		lblInputSelection.setBounds(10, 10, 418, 15);
+		lblInputSelection.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
 		lblInputSelection
 				.setText("Input Selection (mark Text and right click to create columns)");
 
 		mText = new Text(container, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL
 				| SWT.CANCEL | SWT.MULTI);
+		GridData gd_mText = new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1);
+		gd_mText.heightHint = 154;
+		gd_mText.widthHint = 506;
+		mText.setLayoutData(gd_mText);
 		mText.setFont(SWTResourceManager.getFont("Courier", 9, SWT.NORMAL));
-		mText.setBounds(10, 31, 493, 152);
 
 		Menu menu = new Menu(mText);
 		mText.setMenu(menu);
 
 		MenuItem mntmAddColumn = new MenuItem(menu, SWT.NONE);
 		mntmAddColumn.setText("new Column");
+				
+						Group grpColumns = new Group(container, SWT.NONE);
+						GridData gd_grpColumns = new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1);
+						gd_grpColumns.heightHint = 156;
+						grpColumns.setLayoutData(gd_grpColumns);
+						grpColumns.setText("Columns");
+						
+								tableViewer = new TableViewer(grpColumns, SWT.BORDER
+										| SWT.FULL_SELECTION);
+								mTable = tableViewer.getTable();
+								mTable.setBounds(10, 18, 232, 132);
+								
+										TableViewerColumn tableViewerColumn = new TableViewerColumn(
+												tableViewer, SWT.NONE);
+										TableColumn tblclmnAttribute = tableViewerColumn.getColumn();
+										tblclmnAttribute.setWidth(170);
+										tblclmnAttribute.setText("Attribute");
+										
+												Menu menu_1 = new Menu(mTable);
+												mTable.setMenu(menu_1);
+												
+														MenuItem mntmDelete = new MenuItem(menu_1, SWT.NONE);
+														mntmDelete.setText("Delete");
+														mntmDelete.addSelectionListener(new SelectionAdapter() {
+															SelectionUtility su = new SelectionUtility(null);
 
-		Group grpAttributes = new Group(container, SWT.NONE);
-		grpAttributes.setText("Properties");
-		grpAttributes.setBounds(239, 197, 262, 166);
+															@Override
+															public void widgetSelected(SelectionEvent e) {
+																ISelection selection = tableViewer.getSelection();
+																if (selection == null) {
+																	return;
+																}
+																su.setSelection(selection);
+																IVirtualCell c = su.getFirstSelectedAs(IVirtualCell.class);
+																mCells.remove(c);
+																tableViewer.setInput(mCells);
 
-		mTxtSeq = new Text(grpAttributes, SWT.BORDER);
-		mTxtSeq.setBounds(71, 75, 76, 21);
-		mTxtSeq.setEnabled(false);
+															}
+														});
+														
+																tableViewer.setLabelProvider(new TableLabelProviderAdapter() {
+																	@Override
+																	public String getColumnText(Object element, int columnIndex) {
+																		return ((IVirtualCell) element).getName();
+																	}
+																});
+																tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+																
+																		tableViewer
+																				.addSelectionChangedListener(new ISelectionChangedListener() {
+																					SelectionUtility su = new SelectionUtility(null);
+																
+																					@Override
+																					public void selectionChanged(SelectionChangedEvent event) {
+																						su.setSelection(event.getSelection());
+																						IVirtualCell c = su
+																								.getFirstSelectedAs(IVirtualCell.class);
+																						mSelectedCell = c;
+																						if (c == null) {
+																							mTxtDelim.setEnabled(false);
+																							mTxtSeq.setEnabled(false);
+																							mTxtName.setEnabled(false);
+																							return;
+																						}
+																						mTxtDelim.setEnabled(true);
+																						mTxtSeq.setEnabled(true);
+																						mTxtName.setEnabled(true);
+																						if (c.getDelimiter() != null) {
+																							mTxtDelim.setText(c.getDelimiter());
+																						} else {
+																							mTxtDelim.setText("");
+																						}
+																						if (c.getDelimiterSequence() != -1) {
+																							mTxtSeq.setText(c.getDelimiterSequence() + "");
+																						} else {
+																							mTxtSeq.setText("");
+																						}
+																						if (c.getName() != null) {
+																							mTxtName.setText(c.getName());
+																							tableViewer.refresh();
+																						} else {
+																							mTxtName.setText("");
+																							tableViewer.refresh();
+																						}
+																
+																						mText.setSelection(c.getStart(), c.getStop());
+																
+																					}
+																				});
+		
+				Group grpAttributes = new Group(container, SWT.NONE);
+				GridData gd_grpAttributes = new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1);
+				gd_grpAttributes.heightHint = 157;
+				grpAttributes.setLayoutData(gd_grpAttributes);
+				grpAttributes.setText("Properties");
+				
+						mTxtSeq = new Text(grpAttributes, SWT.BORDER);
+						mTxtSeq.setBounds(71, 75, 76, 21);
+						mTxtSeq.setEnabled(false);
+						
+								mTxtDelim = new Text(grpAttributes, SWT.BORDER);
+								mTxtDelim.setBounds(71, 48, 76, 21);
+								mTxtDelim.setEnabled(false);
+								
+										Label lblMatch = new Label(grpAttributes, SWT.NONE);
+										lblMatch.setBounds(10, 78, 55, 15);
+										lblMatch.setText("Sequence");
+										
+												Label lblDelimiter = new Label(grpAttributes, SWT.NONE);
+												lblDelimiter.setBounds(10, 51, 55, 15);
+												lblDelimiter.setText("Delimiter");
+												
+														Label lblName = new Label(grpAttributes, SWT.NONE);
+														lblName.setBounds(10, 23, 55, 15);
+														lblName.setText("Name");
+														
+																mTxtName = new Text(grpAttributes, SWT.BORDER);
+																mTxtName.setBounds(71, 23, 76, 21);
+																mTxtName.setEnabled(false);
+																mTxtDelim.addModifyListener(new ModifyListener() {
 
-		mTxtDelim = new Text(grpAttributes, SWT.BORDER);
-		mTxtDelim.setBounds(71, 48, 76, 21);
-		mTxtDelim.setEnabled(false);
+																	@Override
+																	public void modifyText(ModifyEvent e) {
+																		getSelectedCell().setDelimiter(mTxtDelim.getText());
 
-		Label lblMatch = new Label(grpAttributes, SWT.NONE);
-		lblMatch.setBounds(10, 78, 55, 15);
-		lblMatch.setText("Sequence");
+																	}
+																});
+																mTxtSeq.addModifyListener(new ModifyListener() {
 
-		Label lblDelimiter = new Label(grpAttributes, SWT.NONE);
-		lblDelimiter.setBounds(10, 51, 55, 15);
-		lblDelimiter.setText("Delimiter");
+																	@Override
+																	public void modifyText(ModifyEvent e) {
+																		int i = 0;
+																		try {
+																			i = Integer.parseInt(mTxtSeq.getText());
+																		} catch (NumberFormatException e0) {
+																			return;
+																		}
+																		getSelectedCell().setDelimiterSequence(i);
 
-		Label lblName = new Label(grpAttributes, SWT.NONE);
-		lblName.setBounds(10, 23, 55, 15);
-		lblName.setText("Name");
-
-		mTxtName = new Text(grpAttributes, SWT.BORDER);
-		mTxtName.setBounds(71, 23, 76, 21);
-		mTxtName.setEnabled(false);
-
-		tableViewer = new TableViewer(container, SWT.BORDER
-				| SWT.FULL_SELECTION);
-		mTable = tableViewer.getTable();
-		mTable.setBounds(22, 217, 199, 135);
-
-		TableViewerColumn tableViewerColumn = new TableViewerColumn(
-				tableViewer, SWT.NONE);
-		TableColumn tblclmnAttribute = tableViewerColumn.getColumn();
-		tblclmnAttribute.setWidth(170);
-		tblclmnAttribute.setText("Attribute");
-
-		Menu menu_1 = new Menu(mTable);
-		mTable.setMenu(menu_1);
-
-		MenuItem mntmDelete = new MenuItem(menu_1, SWT.NONE);
-		mntmDelete.setText("Delete");
-
-		Group grpColumns = new Group(container, SWT.NONE);
-		grpColumns.setText("Columns");
-		grpColumns.setBounds(10, 197, 224, 166);
-		mntmDelete.addSelectionListener(new SelectionAdapter() {
-			SelectionUtility su = new SelectionUtility(null);
-
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				ISelection selection = tableViewer.getSelection();
-				if (selection == null) {
-					return;
-				}
-				su.setSelection(selection);
-				IVirtualCell c = su.getFirstSelectedAs(IVirtualCell.class);
-				mCells.remove(c);
-				tableViewer.setInput(mCells);
-
-			}
-		});
-
-		tableViewer.setLabelProvider(new TableLabelProviderAdapter() {
-			@Override
-			public String getColumnText(Object element, int columnIndex) {
-				return ((IVirtualCell) element).getName();
-			}
-		});
-		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
-
-		tableViewer
-				.addSelectionChangedListener(new ISelectionChangedListener() {
-					SelectionUtility su = new SelectionUtility(null);
-
-					@Override
-					public void selectionChanged(SelectionChangedEvent event) {
-						su.setSelection(event.getSelection());
-						IVirtualCell c = su
-								.getFirstSelectedAs(IVirtualCell.class);
-						mSelectedCell = c;
-						if (c == null) {
-							mTxtDelim.setEnabled(false);
-							mTxtSeq.setEnabled(false);
-							mTxtName.setEnabled(false);
-							return;
-						}
-						mTxtDelim.setEnabled(true);
-						mTxtSeq.setEnabled(true);
-						mTxtName.setEnabled(true);
-						if (c.getDelimiter() != null) {
-							mTxtDelim.setText(c.getDelimiter());
-						} else {
-							mTxtDelim.setText("");
-						}
-						if (c.getDelimiterSequence() != -1) {
-							mTxtSeq.setText(c.getDelimiterSequence() + "");
-						} else {
-							mTxtSeq.setText("");
-						}
-						if (c.getName() != null) {
-							mTxtName.setText(c.getName());
-							tableViewer.refresh();
-						} else {
-							mTxtName.setText("");
-							tableViewer.refresh();
-						}
-
-						mText.setSelection(c.getStart(), c.getStop());
-
-					}
-				});
-		mTxtDelim.addModifyListener(new ModifyListener() {
-
-			@Override
-			public void modifyText(ModifyEvent e) {
-				getSelectedCell().setDelimiter(mTxtDelim.getText());
-
-			}
-		});
-		mTxtSeq.addModifyListener(new ModifyListener() {
-
-			@Override
-			public void modifyText(ModifyEvent e) {
-				int i = 0;
-				try {
-					i = Integer.parseInt(mTxtSeq.getText());
-				} catch (NumberFormatException e0) {
-					return;
-				}
-				getSelectedCell().setDelimiterSequence(i);
-
-			}
-		});
-
-		mTxtName.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent e) {
-				getSelectedCell().setName(mTxtName.getText());
-				tableViewer.refresh();
-
-			}
-		});
+																	}
+																});
+																
+																		mTxtName.addKeyListener(new KeyAdapter() {
+																			@Override
+																			public void keyReleased(KeyEvent e) {
+																				getSelectedCell().setName(mTxtName.getText());
+																				tableViewer.refresh();
+																
+																			}
+																		});
 		mntmAddColumn.addSelectionListener(this);
 
 		return container;
