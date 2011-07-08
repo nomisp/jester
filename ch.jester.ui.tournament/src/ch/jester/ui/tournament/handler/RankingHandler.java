0499cc77ce471fe5411a7af7b8cd3248990c46da
matthias.liechti <matthias.liechti@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-07-08 21:39:21 +0000

    Vorbereitung Report Aufruf für RankingListe (Report fehlt noch) - Aufruf noch deaktiviert

--- ch.jester.ui.tournament/src/ch/jester/ui/tournament/handler/RankingHandler.java
+++ ch.jester.ui.tournament/src/ch/jester/ui/tournament/handler/RankingHandler.java
@@ -2,6 +2,7 @@ package ch.jester.ui.tournament.handler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.IHandler;
@@ -17,16 +18,20 @@ import org.eclipse.ui.navigator.CommonNavigator;
 import org.eclipse.ui.progress.UIJob;
 
 import ch.jester.common.ui.handlers.AbstractCommandHandler;
+import ch.jester.common.ui.utility.UIUtility;
 import ch.jester.common.utility.ExceptionUtility;
 import ch.jester.commonservices.util.ServiceUtility;
 import ch.jester.model.Category;
+import ch.jester.model.Ranking;
 import ch.jester.model.RankingSystem;
 import ch.jester.model.Tournament;
 import ch.jester.system.api.ranking.IRankingSystem;
 import ch.jester.system.api.ranking.IRankingSystemEntry;
 import ch.jester.system.api.ranking.IRankingSystemManager;
 import ch.jester.system.exceptions.NotAllResultsException;
+import ch.jester.ui.round.editors.RankingViewEditor;
 import ch.jester.ui.tournament.cnf.TournamentNavigator;
+import ch.jester.ui.tournament.internal.RankingEntityHelp;
 import ch.jester.ui.tournament.nl1.Messages;
 
 /**
@@ -70,11 +75,26 @@ public class RankingHandler extends AbstractCommandHandler implements IHandler {
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
 				try {
+					final RankingEntityHelp helpentity;
 					if (cat == null) {
-						primaryRankingSystem.calculateRanking(tournament, monitor);	// TODO Peter: Evtl. für kommende Version mehrere Feinwertungen
+						Map<Category, Ranking> rankingMap = primaryRankingSystem.calculateRanking(tournament, monitor);	// TODO Peter: Evtl. für kommende Version mehrere Feinwertungen
+						helpentity=new RankingEntityHelp(rankingMap);
 					} else {
-						primaryRankingSystem.calculateRanking(cat, monitor);	// TODO Peter: Evtl. für kommende Version mehrere Feinwertungen
+						Ranking ranking = primaryRankingSystem.calculateRanking(cat, monitor);	// TODO Peter: Evtl. für kommende Version mehrere Feinwertungen
+						helpentity=new RankingEntityHelp(cat, ranking);
+					
 					}
+					UIUtility.syncExecInUIThread(new Runnable(){
+
+						@Override
+						public void run() {
+							//openEditor(helpentity, RankingViewEditor.ID);
+						}
+						
+						
+					});
+					
+					
 					
 				} catch (Exception e) {
 					Throwable exception = ExceptionUtility.getRealException(e);
