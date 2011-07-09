c949e2530f8b6c24013d7a14022dc6dbe090327b
matthias.liechti <matthias.liechti@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-07-09 16:51:14 +0000

    gesichtsbuch

---
+++ trunk/ch.jester.socialmedia.facebook/src/ch/jester/socialmedia/facebook/FacebookService.java
@@ -0,0 +1,81 @@
+package ch.jester.socialmedia.facebook;
+
+import java.util.Set;
+
+import ch.jester.commonservices.api.preferences.IPreferenceProperty;
+import ch.jester.socialmedia.api.ISocialStatusUpdater;
+import ch.jester.socialmedia.auth.OAuthServiceComponent;
+import ch.jester.socialmedia.facebook.internal.authflow.FacebookAuthorisationFlow;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import com.restfb.types.FacebookType;
+
+public class FacebookService extends OAuthServiceComponent implements ISocialStatusUpdater{
+	private FacebookClient facebookClient;
+	private FacebookAuthorisationFlow accessFlow;
+	private boolean sendRequest = true;
+	private IPreferenceProperty expIn, expInTimeStamp;
+	@Override
+	public void initializeService() {
+		Set<IPreferenceProperty> props = mPreferenceManager.getProperties();
+		for(IPreferenceProperty p:props){
+			p.setEnabled(false);
+		}
+		//mAuthAccessToken.setEnabled(true);
+		initializeAuthorisationFlow();
+		
+	}
+	@Override
+	protected void createAdditionalProperties() {
+		expIn = mPreferenceManager.create("expiresIn", "TokenExpiresIn", 0).setEnabled(false);
+		expInTimeStamp = mPreferenceManager.create("expiresInTimeStamp", "Token received", 0L).setEnabled(false);
+		expInTimeStamp.setType(Long.class);
+	}
+	private void initializeAuthorisationFlow() {
+		accessFlow = new FacebookAuthorisationFlow(mAuthAccessToken.getStringValue(), expIn.getIntegerValue(), (Long)expInTimeStamp.getValue());
+		
+	}
+
+	private void checkCredentials() {
+		if(accessFlow.established()){return;}
+		accessFlow.authorizeUser();
+		mAuthAccessToken.setValue(accessFlow.getAccessToken());
+		expIn.setValue(accessFlow.getExpiresIn());
+		expInTimeStamp.setValue(accessFlow.getExpiresInTimeStamp());
+		facebookClient = new DefaultFacebookClient(mAuthAccessToken.getStringValue());
+		/*if(getAuthAccessToken()==null || getAuthAccessToken().isEmpty()){
+			accessFlow = new FacebookAuthorisationFlow();
+			String token = FacebookAuthorisationFlow.accessToken;
+			int expires = FacebookAuthorisationFlow.expires;
+			setAuthAccessToken(token);
+			mPreferenceManager.getPropertyByInternalKey("expires").setValue(expires);
+		}
+		String token = getAuthAccessToken();
+		String u = token;
+		facebookClient = new DefaultFacebookClient(u);*/
+	}
+	@Override
+	public void updateStatus(String pStatus){
+		checkCredentials();
+
+		if(!sendRequest){return;}
+		FacebookType publishMessageResponse =
+			  facebookClient.publish("me/feed", FacebookType.class,
+			    Parameter.with("message", pStatus));
+	}
+	@Override
+	public int getMaxCharacterForStatus() {
+		return 160;
+	}
+	public void checkAuthorisation() {
+		checkCredentials();
+		
+	}
+	@Override
+	protected String getPreferenceKey() {
+		return "ch.jester.socialmedia.facebook";
+	}
+
+}
