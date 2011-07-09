a669c85230b15bb11aefd97fbf384f0acc8f98c5
nomisp@gmail.com <nomisp@gmail.com@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-07-09 21:20:58 +0000

    Sonneborn-Berger Feinwertung

---
+++ ch.jester.rankingsystem.sonnebornberger/src/ch/jester/rankingsystem/sonnebornberger/internal/SonnebornBergerActivator.java
@@ -0,0 +1,34 @@
+package ch.jester.rankingsystem.sonnebornberger.internal;
+
+import org.osgi.framework.BundleContext;
+
+import ch.jester.common.activator.AbstractActivator;
+
+public class SonnebornBergerActivator extends AbstractActivator {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "ch.jester.rankingsystem.sonnebornberger"; //$NON-NLS-1$
+	// The shared instance
+	private static SonnebornBergerActivator plugin;
+	
+	@Override
+	public void startDelegate(BundleContext pContext) {
+		plugin = this;
+		getActivationContext().getLogger().info("Starting SonnebornBerger Ranking-System Plugin");
+	}
+
+	@Override
+	public void stopDelegate(BundleContext pContext) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static SonnebornBergerActivator getDefault() {
+		return plugin;
+	}
+}
