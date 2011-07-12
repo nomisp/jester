55a5f70358cf87cbfcec848cda4231a0d0ca8799
matthias.liechti@gmail.com <matthias.liechti@gmail.com@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-07-12 10:42:28 +0000

    - Target Update hoffentlich (die richtigen Libs) - Proxy Einstellungen nun über Eclipse Default (org.eclipse.ui.net), so dass sie auch für den Update Manager funzen

--- ch.jester.common/src/ch/jester/common/web/HTTPFactory.java
+++ ch.jester.common/src/ch/jester/common/web/HTTPFactory.java
@@ -6,12 +6,43 @@ import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
 
+import org.eclipse.core.internal.net.ProxyData;
+import org.eclipse.core.internal.net.ProxyManager;
+import org.eclipse.core.net.proxy.IProxyData;
+import org.eclipse.core.net.proxy.IProxyService;
+
+import ch.jester.commonservices.util.ServiceUtility;
+
 public class HTTPFactory {
 	private static Proxy mProxy; 
+	private static ServiceUtility su = new ServiceUtility();
 	public static void createHTTPProxy(String pProxyAdress, int pProxyPort) {
 		mProxy =  new Proxy(Proxy.Type.HTTP, new InetSocketAddress(pProxyAdress,
 				pProxyPort));
+
 	}
+	public static void createHTTPProxyFromEnvSettings(){
+		String host = System.getProperty("http.proxyHost");
+		String port = System.getProperty("http.proxyPort");
+		if(host==null&&port==null){
+			reset();
+			return;
+		}
+		createHTTPProxy(host, Integer.parseInt(port));
+	}
+	public static void createHTTPProxy(){
+		IProxyService service = su.getService(IProxyService.class);
+		if(service!=null){
+			IProxyData da = service.getProxyData("HTTP");
+			if(service.isProxiesEnabled()){
+				createHTTPProxy(da.getHost(), da.getPort());
+			}
+			createHTTPProxyFromEnvSettings();
+		}else{
+			createHTTPProxyFromEnvSettings();
+		}
+	}
+	
 	public static Proxy getHTTPProxy(){
 		return mProxy;
 	}
@@ -20,11 +51,14 @@ public class HTTPFactory {
 	}
 	public static HttpURLConnection connect(String pUrl, boolean open) throws IOException{
 		URL url = new URL(pUrl);
+		createHTTPProxy();
 		HttpURLConnection uc = null;
+
 		if(HTTPFactory.getHTTPProxy()==null){
 			uc = (HttpURLConnection) url.openConnection();
 		}else{
 			uc =  (HttpURLConnection) url.openConnection(HTTPFactory.getHTTPProxy());
+			
 		}
 		if(open){
 			uc.connect();
