96399ae8af7f3e4ad2f85f7f3324756aead5ea7a
matthias.liechti <matthias.liechti@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-07-16 08:30:13 +0000

    warnings entfernt

--- trunk/ch.jester.db.persister.impl/src/ch/jester/db/persister/impl/Activator.java
+++ trunk/ch.jester.db.persister.impl/src/ch/jester/db/persister/impl/Activator.java
@@ -4,11 +4,6 @@ import org.osgi.framework.BundleContext;
 
 import ch.jester.common.activator.AbstractActivator;
 import ch.jester.commonservices.api.persistency.IDaoServiceFactory;
-import ch.jester.commonservices.util.ServiceUtility;
-import ch.jester.dao.ICategoryDao;
-import ch.jester.dao.IPlayerDao;
-import ch.jester.dao.IRoundDao;
-import ch.jester.dao.ITournamentDao;
 
 public class Activator extends AbstractActivator{
 	IDaoServiceFactory factory;
@@ -22,12 +17,7 @@ public class Activator extends AbstractActivator{
 	
 	@Override
 	public void startDelegate(BundleContext pContext) {
-		ServiceUtility utility = getActivationContext().getServiceUtil();
-		IDaoServiceFactory factory = utility.getService(IDaoServiceFactory.class);
-		//factory.addServiceHandling(IPlayerDao.class, DBPlayerPersister.class);
-		//factory.addServiceHandling(ITournamentDao.class, DBTournamentPersister.class);
-		//factory.addServiceHandling(ICategoryDao.class, DBCategoryPersister.class);
-		//factory.addServiceHandling(IRoundDao.class, DBRoundPersister.class);
+
 	}
 
 	@Override
