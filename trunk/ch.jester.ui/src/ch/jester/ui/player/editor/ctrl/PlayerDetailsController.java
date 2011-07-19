feb3e86fbb0d7d31e4aebb37130a91dbd5b62402
matthias.liechti@gmail.com <matthias.liechti@gmail.com@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-07-19 12:14:38 +0000

    Validation

--- trunk/ch.jester.ui/src/ch/jester/ui/player/editor/ctrl/PlayerDetailsController.java
+++ trunk/ch.jester.ui/src/ch/jester/ui/player/editor/ctrl/PlayerDetailsController.java
@@ -16,6 +16,7 @@ import org.eclipse.core.databinding.observable.map.IObservableMap;
 import ch.jester.model.Club;
 import org.eclipse.jface.databinding.viewers.ObservableMapLabelProvider;
 import org.eclipse.core.databinding.observable.list.WritableList;
+import ch.jester.ui.forms.TestValidator;
 
 
 
@@ -79,39 +80,41 @@ public class PlayerDetailsController {
 		//
 		IObservableValue mPlayerDetailgetTxtAgeObserveTextObserveWidget = SWTObservables.observeText(mPlayerDetail.getTxtAge(), SWT.Modify);
 		IObservableValue playerAgeObserveValue = BeansObservables.observeValue(player, "age");
-		bindingContext.bindValue(mPlayerDetailgetTxtAgeObserveTextObserveWidget, playerAgeObserveValue, null, null);
+		UpdateValueStrategy strategy = new UpdateValueStrategy(UpdateValueStrategy.POLICY_ON_REQUEST);
+		UpdateValueStrategy strategy_1 = new UpdateValueStrategy();
+		bindingContext.bindValue(mPlayerDetailgetTxtAgeObserveTextObserveWidget, playerAgeObserveValue, strategy, strategy_1);
 		//
 		IObservableValue cityObserveWidget = SWTObservables.observeText(mPlayerDetail.getCityText(), SWT.Modify);
 		IObservableValue cityObserveValue = BeansObservables.observeValue(player, "city");
-		bindingContext.bindValue(cityObserveWidget, cityObserveValue, null, null);
+		bindingContext.bindValue(cityObserveWidget, cityObserveValue, new UpdateValueStrategy(UpdateValueStrategy.POLICY_ON_REQUEST), null);
 		//
 		IObservableValue nationObserveWidget = SWTObservables.observeText(mPlayerDetail.getNationText(), SWT.Modify);
 		IObservableValue nationObserveValue = BeansObservables.observeValue(player, "nation");
-		bindingContext.bindValue(nationObserveWidget, nationObserveValue, null, null);
+		bindingContext.bindValue(nationObserveWidget, nationObserveValue, new UpdateValueStrategy(UpdateValueStrategy.POLICY_ON_REQUEST), null);
 		//
 		IObservableValue mPlayerDetailgetFideCodeTextObserveTextObserveWidget = SWTObservables.observeText(mPlayerDetail.getFideCodeText(), SWT.Modify);
 		IObservableValue playerFideCodeObserveValue = BeansObservables.observeValue(player, "fideCode");
-		bindingContext.bindValue(mPlayerDetailgetFideCodeTextObserveTextObserveWidget, playerFideCodeObserveValue, null, null);
+		bindingContext.bindValue(mPlayerDetailgetFideCodeTextObserveTextObserveWidget, playerFideCodeObserveValue, new UpdateValueStrategy(UpdateValueStrategy.POLICY_ON_REQUEST), null);
 		//
 		IObservableValue mPlayerDetailgetNationalCodeTextObserveTextObserveWidget = SWTObservables.observeText(mPlayerDetail.getNationalCodeText(), SWT.Modify);
 		IObservableValue playerNationalCodeObserveValue = BeansObservables.observeValue(player, "nationalCode");
-		bindingContext.bindValue(mPlayerDetailgetNationalCodeTextObserveTextObserveWidget, playerNationalCodeObserveValue, null, null);
+		bindingContext.bindValue(mPlayerDetailgetNationalCodeTextObserveTextObserveWidget, playerNationalCodeObserveValue, new UpdateValueStrategy(UpdateValueStrategy.POLICY_ON_REQUEST), null);
 		//
 		IObservableValue mPlayerDetailgetEloTextObserveTextObserveWidget = SWTObservables.observeText(mPlayerDetail.getEloText(), SWT.Modify);
 		IObservableValue playerEloObserveValue = BeansObservables.observeValue(player, "elo");
-		bindingContext.bindValue(mPlayerDetailgetEloTextObserveTextObserveWidget, playerEloObserveValue, null, null);
+		bindingContext.bindValue(mPlayerDetailgetEloTextObserveTextObserveWidget, playerEloObserveValue, new UpdateValueStrategy(UpdateValueStrategy.POLICY_ON_REQUEST), null);
 		//
 		IObservableValue mPlayerDetailgetNationalEloTextObserveTextObserveWidget = SWTObservables.observeText(mPlayerDetail.getNationalEloText(), SWT.Modify);
 		IObservableValue playerNationalEloObserveValue = BeansObservables.observeValue(player, "nationalElo");
-		bindingContext.bindValue(mPlayerDetailgetNationalEloTextObserveTextObserveWidget, playerNationalEloObserveValue, null, null);
+		bindingContext.bindValue(mPlayerDetailgetNationalEloTextObserveTextObserveWidget, playerNationalEloObserveValue, new UpdateValueStrategy(UpdateValueStrategy.POLICY_ON_REQUEST), null);
 		//
 		IObservableValue mPlayerDetailgetTextObserveTextObserveWidget = SWTObservables.observeText(mPlayerDetail.getText(), SWT.Modify);
 		IObservableValue playerEstimatedEloObserveValue = BeansObservables.observeValue(player, "estimatedElo");
-		bindingContext.bindValue(mPlayerDetailgetTextObserveTextObserveWidget, playerEstimatedEloObserveValue, null, null);
+		bindingContext.bindValue(mPlayerDetailgetTextObserveTextObserveWidget, playerEstimatedEloObserveValue, new UpdateValueStrategy(UpdateValueStrategy.POLICY_ON_REQUEST), null);
 		//
 		IObservableValue mPlayerDetailgetTxtTitleObserveSingleSelection = ViewersObservables.observeSingleSelection(mPlayerDetail.getTxtTitle());
 		IObservableValue playerTitleObserveValue = BeansObservables.observeValue(player, "title");
-		bindingContext.bindValue(mPlayerDetailgetTxtTitleObserveSingleSelection, playerTitleObserveValue, null, null);
+		bindingContext.bindValue(mPlayerDetailgetTxtTitleObserveSingleSelection, playerTitleObserveValue, new UpdateValueStrategy(UpdateValueStrategy.POLICY_ON_REQUEST), null);
 		//
 		ObservableListContentProvider listContentProvider = new ObservableListContentProvider();
 		mPlayerDetail.getListViewer().setContentProvider(listContentProvider);
