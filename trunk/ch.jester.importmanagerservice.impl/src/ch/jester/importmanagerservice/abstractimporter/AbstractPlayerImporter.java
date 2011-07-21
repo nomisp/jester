2060eb528fbd9eec93c822a7281b76eb19f2d0b6
matthias.liechti <matthias.liechti@87b5f25c-4103-4d20-a5e3-630113a02ee7>
2011-07-21 17:33:26 +0000

    Übersetzung Importer Player properties

--- trunk/ch.jester.importmanagerservice.impl/src/ch/jester/importmanagerservice/abstractimporter/AbstractPlayerImporter.java
+++ trunk/ch.jester.importmanagerservice.impl/src/ch/jester/importmanagerservice/abstractimporter/AbstractPlayerImporter.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.persistence.Query;
 
+import messages.Messages;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 
 import ch.jester.common.importer.AbstractTableImporter;
@@ -17,9 +19,26 @@ import ch.jester.model.factories.ModelFactory;
 /**
  */
 public abstract class AbstractPlayerImporter<T> extends AbstractTableImporter<T, Player> implements IDuplicateChecker<Player> {
-	String[] exposedProperties = new String[]{"lastName","firstName","fideCode","nationalCode","elo","nationalElo","age","city","nation"};
-	String[] exposedLabels = new String[]{"Lastname","Firstname","Fide code","National code","Elo","National Elo","Age","City","Nation"};
+	String[] exposedProperties = new String[]{"lastName","firstName","fideCode","nationalCode","elo","nationalElo","estimatedElo","age","city","nation","club"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$
+	String[] exposedLabels;
+	/*
+	 * 		mPropertyTranslator.create("club", "Club");
+		mPropertyTranslator.create("estimatedElo", "Estimated Elo");
+	 * 
+	 */
 	public AbstractPlayerImporter(){
+		 exposedLabels= new String[]{
+				 Messages.AbstractPlayerImporter_lastname,
+				 Messages.AbstractPlayerImporter_firstname,
+				 Messages.AbstractPlayerImporter_fidecode,
+				 Messages.AbstractPlayerImporter_nationalcode,
+				 Messages.AbstractPlayerImporter_elo,
+				 Messages.AbstractPlayerImporter_nationalelo,
+				 Messages.AbstractPlayerImporter_estimatedelo,
+				 Messages.AbstractPlayerImporter_age,
+				 Messages.AbstractPlayerImporter_city,
+				 Messages.AbstractPlayerImporter_nation,
+				 Messages.AbstractPlayerImporter_club};
 		for(int i=0;i<exposedLabels.length;i++){
 			mPropertyTranslator.create(exposedProperties[i], exposedLabels[i]);
 		}
@@ -49,19 +68,19 @@ public abstract class AbstractPlayerImporter<T> extends AbstractTableImporter<T,
 		checker.manualEventQueueNotification(true);
 		boolean checkDoubleEntries = checkDuplicates(checker);
 		Query duplQuery = createDuplicationCheckingQuery(checker);
-		duplQuery.setHint("org.hibernate.cacheable", true);
+		duplQuery.setHint("org.hibernate.cacheable", true); //$NON-NLS-1$
 		int chunkCount = 0;
 		while(iterator.hasNext()){
 			List<Player> origList = iterator.next();
 			List<Player> duplicates=null;
 			if(checkDoubleEntries){
-				pMonitor.subTask("Checking Duplicates");
+				pMonitor.subTask(Messages.AbstractPlayerImporter_checkingduplicates);
 				List<?> codes = getDuplicationKeys(origList);
 				duplicates = duplQuery.setParameter(getParameter(), codes).getResultList();
-				getLogger().info("Duplicates found >>>>"+duplicates.size());
+				getLogger().info("Duplicates found >>>>"+duplicates.size()); //$NON-NLS-1$
 				
 			}
-			pMonitor.subTask("Saving Players: "+(chunkSize*chunkCount)+" - "+(chunkSize*(chunkCount+1)));
+			pMonitor.subTask(Messages.AbstractPlayerImporter_savingplayers+(chunkSize*chunkCount)+" - "+(chunkSize*(chunkCount+1))); //$NON-NLS-2$
 			
 			if(duplicates!=null){
 				origList.removeAll(duplicates);
